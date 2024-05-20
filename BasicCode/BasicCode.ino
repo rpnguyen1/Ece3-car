0be35e3aafec77b4bd02e79df7c71a2a6e164238
Richard Nguyen <nguyenprichard@gmail.com>
2024-05-20 13:51:40 -0700

    problem
    try to spin but doesnt work

--- BasicCode/BasicCode.ino
+++ BasicCode/BasicCode.ino
@@ -20,10 +20,13 @@ const int LED_RF = 41;
 uint16_t sensorValues[8];
 
 
-int leftBaseSpd = 42; // base
-int rightBaseSpd = 42; // base
+int leftBaseSpd = 30; // base
+int rightBaseSpd = 30; // base
 int newSpeedL = 0;
 int newSpeedR = 0;
+
+int crossSpeedFlag = 0;
+int spinState = 0;
 
 
 ///////////////////////////////////
@@ -43,16 +46,17 @@ void setup() {
   digitalWrite(right_dir_pin,LOW);
   digitalWrite(right_nslp_pin,HIGH);
 
-  pinMode(LED_RF, OUTPUT);
-  
+  // pinMode(LED_RF, OUTPUT);
+  crossSpeedFlag = 0;
   ECE3_Init();
   // set the data rate in bits/second for serial data transmission
   Serial.begin(9600); 
+  
   delay(2000); //Wait 2 seconds before starting 
 
   // leds
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_BUILTIN_2, OUTPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(LED_BUILTIN_2, OUTPUT);
   
 }
 
@@ -96,6 +100,42 @@ void loop() {
     normalized_values[i] = ((value * 1000) - (min[i] * 1000)) / (max[i] - min[i]) ; // multiply by 1000 first to avoid int rounding
   }
 
+  // if ((normalized_values[0] > 300 || normalized_values[1] > 300) 
+  //   && (normalized_values[2] > 500 || normalized_values[3] > 500 || normalized_values[4] > 500 ) 
+  //   && (normalized_values[6] > 300 || normalized_values[7] > 300)){
+  //   normalized_values[0] = 0;
+  //   normalized_values[1] = 0;
+  //   normalized_values[6] = 0;
+  //   normalized_values[7] = 0;
+  // }
+
+
+  if (normalized_values[0] > 990 && 
+    normalized_values[1] > 990 && 
+    normalized_values[2] > 990 && 
+    normalized_values[3] > 990 && 
+    normalized_values[4] > 990 && 
+    normalized_values[5] > 990 && 
+    normalized_values[6] > 990 && 
+    normalized_values[7] > 990){
+
+    crossSpeedFlag = crossSpeedFlag + 1;
+
+  }
+
+  if (crossSpeedFlag >= 2){
+    if (spinState = 1){
+      spinState = 0;
+    }else if (spinState = 0){
+      spinState = 1;
+    }
+  }
+  // int numInflections = 0;
+  // int prevValue = sensorValues[0];
+  // for (unsigned char i = 0; i < 8; i++)
+  // {
+  //   value = sensorValues[i];
+  // }
   // }
   // Serial.println();
   int calc = calc8421(normalized_values[0], 
@@ -115,19 +155,19 @@ void loop() {
 //////////////////////////////////
 
   // add error to error array and shift
-  // error[0] = error[1];
-  // error[1] = calc;
+  error[0] = error[1];
+  error[1] = calc;
 
-  float k_p = 0.05;
+  float k_p = 0.04;
   int p = calc * k_p;
 
-  // float k_c = 0.05;
-  // int delta_error = error[1] - error[0];
-  // int c = delta_error * k_c;
+  float k_d = 0.3;
+  int delta_error = error[1] - error[0];
+  int d = delta_error * k_d;
 
 ///////////////////////
-  // Serial.print("     c ");
-  // Serial.print(c);
+  // Serial.print("     d ");
+  // Serial.print(d);
   // Serial.print("     delta_error ");
   // Serial.print(delta_error);
 
@@ -136,15 +176,75 @@ void loop() {
 
   // Serial.print("     error[1] ");
   // Serial.print(error[1]);
-  //   Serial.println();
 
   // Serial.println();
   // calc new speed (p)
+  int steering = 0;
 
-  newSpeedL = leftBaseSpd - p;
-  newSpeedR = rightBaseSpd + p;
+  // if (p>0 && d>0){  // +P   + D
+  //   steering = p + d;
+  // }else if (p<0 && d>0){   // -P  +D
+  //   steering = p + d;
+  // }else if (p>0 && d<0){   // +P  -D
+  //   steering = p + d;    // left side of the line, moving towards
+  // }else{   // -P   -D
+  //   steering = p + d;
+  // }
+  // steering = p + d;
 
-  analogWrite(left_pwm_pin,newSpeedL);
-  analogWrite(right_pwm_pin,newSpeedR);
+  // Serial.print("     steering ");
+  // Serial.print(steering);
+  // Serial.println();
+
+  newSpeedL = leftBaseSpd - (p + d);
+  newSpeedR = rightBaseSpd + (p + d);
+
+
+
+  if (newSpeedL < 0){
+    newSpeedL = 0;
+  }
+  if (newSpeedL > 255){
+    newSpeedL = 255;
+  }
+  if (newSpeedR < 0){
+    newSpeedR = 0;
+  }
+  if (newSpeedR > 255){
+    newSpeedR = 255;
+  }
+  // Serial.print("     newSpeedL ");
+  // Serial.print(newSpeedL);
+  // Serial.print("     newSpeedR ");
+  // Serial.print(newSpeedR);
+  // Serial.println();
+  // check for negative TODO
+  if (spinState = 1){
+    
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN_2, HIGH);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_RF, HIGH);
+    digitalWrite(left_dir_pin,HIGH);
+    digitalWrite(left_nslp_pin,HIGH);
+    digitalWrite(right_dir_pin,LOW);
+    digitalWrite(right_nslp_pin,HIGH);
+
+    analogWrite(left_pwm_pin,newSpeedL);
+    analogWrite(right_pwm_pin,newSpeedR);
+  } else{
+    digitalWrite(LED_BUILTIN, LOW);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN_2, LOW);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_RF, LOW);
+
+    digitalWrite(left_dir_pin,LOW);
+    digitalWrite(left_nslp_pin,HIGH);
+    digitalWrite(right_dir_pin,LOW);
+    digitalWrite(right_nslp_pin,HIGH);
+    
+    analogWrite(left_pwm_pin,newSpeedL);
+    analogWrite(right_pwm_pin,newSpeedR);
+  }
+
+
 }
 
