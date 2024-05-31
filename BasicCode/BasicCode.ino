456c0cafc3f3aaa11b1e66298bfab83292f5919c
Richard Nguyen <nguyenprichard@gmail.com>
2024-05-30 20:36:47 -0700

    max speed spins
    and lights

--- BasicCode/BasicCode.ino
+++ BasicCode/BasicCode.ino
@@ -36,7 +36,7 @@ int normalized_values[8] = {0};
 // int max[8] = {2500, 2500, 2500, 2369, 2440, 2500, 2415}; // Starting values
 int max[8] = {2500, 2500, 2500, 2500, 2500, 2500, 2500}; // Starting values
 // int min[8] = {791, 664, 711, 641, 757, 741, 804};
-int min[8] = {791, 664, 711, 641, 757, 741, 804};
+int min[8] = {791, 664, 711, 641, 757, 741, 741};
 // int min[8] = {591, 464, 511, 441, 557, 441, 604};
 int error[2] = {0,0};
 
@@ -89,94 +89,19 @@ void setup() {
 }
 
 // Function definition
-int calc8421(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, bool sideInterference){
+int calc8421(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8){
   int result;
-  int w[4] = {8, 4, 2, 1};
+  int w[4] = {1, 2, 3, 4};
+  // if (startTicks < 200) {
+  //     w[0] = 4;
+  //     w[1] = 3;
+  //     w[2] = 2;
+  //     w[3] = 1;
+  // }
   // result = (-8 * x1 - 4 * x2 - 2 * x3 - x4 + x5 + 2 * x6 + 4 * x7 + 8 * x8) / 4;
   result = (-1 * w[0] * x1 - w[1] * x2 - w[2] * x3 - w[3] * x4 
                + w[3] * x5 + w[2] * x6 + w[1] * x7 + w[0] * x8) / 4;
   return result;
-  // int weights[7] = {-8, -4, -2, -1, 1, 2, 4};
-  // int weightedSum = 0;
-  // int weightSum = 0;
-  
-  // for (int i = 0; i < 7; i++) {
-  //   int sensorValue = sensorValues[i];
-  //   if (sideInterference && (i == 0 || i == 1 || i == 5 || i == 6)) {
-  //     sensorValue = 0; // Zero out side sensors
-  //   }
-  //   weightedSum += weights[i] * sensorValue;
-  //   weightSum += abs(weights[i]);
-  // }
-  
-  // // Calculate and return the error value
-  // return weightedSum / weightSum;
-}
-// Function to normalize sensor values to binary (0 for white, 1 for black)
-// void normalizeSensors(int normalizedValues[7]) {
-//   for (int i = 0; i < 7; i++) {
-//     binary_values[i] = normalizedValues[i] > threshold ? 1 : 0;
-//   }
-// }
-
-// Function to detect black-white-black-white-black pattern
-// bool detectBWPattern(int normalizedValues[7]) {
-//   int patternCount = 0;
-//   for (int i = 1; i < 6; i++) {
-//     if (binary_values[i-1] != binary_values[i]) {
-//       patternCount++;
-//     }
-//   }
-//   return patternCount == 4; // Expecting exactly 4 transitions for B-W-B-W-B
-// }
-
-// int countInflectionPoints() {
-//   int inflectionPoints = 0;
-//   for (int i = 1; i < 6; i++) {
-//     int change1 = abs(normalized_values[i] - normalized_values[i-1]);
-//     int change2 = abs(normalized_values[i+1] - normalized_values[i]);
-//     if (change1 > changeThreshold && change2 > changeThreshold && (normalized_values[i] - normalized_values[i-1]) * (normalized_values[i+1] - normalized_values[i]) < 0) {
-//       inflectionPoints++;
-//     }
-//   }
-//   return inflectionPoints;
-// }
-
-// // Function to detect side interference based on inflection points
-// bool detectSideInterference() {
-//   int inflectionPoints = countInflectionPoints();
-//   // Assuming that more than 2 inflection points indicate side interference
-//   return inflectionPoints > 2;
-// }
-
-bool detectSideInterference() {
-  // int patternCount = 0;
-  // int firstBlack = 0;
-  // int firstWhite= 0;
-  // int midBlack = 0;
-  // int secWhite = 0;
-  // int thirdBlack = 0;
-  bool ret = false;
-  if (sensorValues[0] > 2490 && 
-    sensorValues[1] > 2490 && 
-    // sensorValues[2] > 2490 && 
-    // sensorValues[3] > 2490 && 
-    // sensorValues[4] > 2490 && 
-    // sensorValues[5] > 2490 && 
-    sensorValues[6] > 2490 && 
-    sensorValues[7] > 2490){
-  //   // for (int i = 0; i < 7; i++) {
-  //   //   if (sensorValues[i] > 2490){
-  //   //     firstBlack = i;
-  //   //   }else{
-  //   //     firstWhite = i;
-  //   //   }
-
-  //   // }
-    ret = true;
-  }
-  return ret;
-  // return patternCount == 4; // Expecting exactly 4 transitions for B-W-B-W-B
 }
 
 void loop() {
@@ -205,37 +130,8 @@ void loop() {
     normalized_values[i] = ((value * 1000) - (min[i] * 1000)) / (max[i] - min[i]) ; // multiply by 1000 first to avoid int rounding
   }
 
-  // bool sideInterference = detectSideInterference();
-  bool sideInterference = false;
-  if (sensorValues[0] > 2490 && 
-      sensorValues[7] > 2490){
-      if ((sensorValues[1] < 2490 && sensorValues[2] > 2490 && 
-          sensorValues[3] < 2490 && sensorValues[4] > 2490 &&  
-          sensorValues[5] < 2490) ||
-          (sensorValues[2] < 2490 && sensorValues[3] > 2490 &&  
-          sensorValues[4] < 2490 && sensorValues[5] > 2490 && 
-          sensorValues[6] < 2490)) {  // W-B
-        sideInterference =  true; // Pattern detected
-      }
-    }
-  if (sideInterference == true){
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    digitalWrite(LED_BUILTIN_2, HIGH);  // turn the LED on (HIGH is the voltage level)
-  }
-
 
     // Serial.println();
-
-  if ((normalized_values[0] > 300 || normalized_values[1] > 300) 
-    && (normalized_values[2] > 500 || normalized_values[3] > 500 || normalized_values[4] > 500 ) 
-    && (normalized_values[6] > 300 || normalized_values[7] > 300))
-    {
-    // normalized_values[0] = 0;
-    // normalized_values[1] = 0;
-    // normalized_values[6] = 0;
-    // normalized_values[7] = 0;
-
-  }
 
 
   if (sensorValues[0] > 2490 && 
@@ -279,6 +175,21 @@ void loop() {
     //   spinState = 1;
     // }
   }
+
+  
+  // if (sensorValues[0] > 2400 && 
+  //   sensorValues[7] > 2400){
+  //   normalized_values[0] = 0;
+  //   normalized_values[1] = 0;
+  //   normalized_values[2] = 0;
+  //   // normalized_values[3] = 0;
+  //   // normalized_values[4] = 0;
+  //   normalized_values[5] = 0;
+  //   normalized_values[6] = 0;
+  //   normalized_values[7] = 0;
+  //   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  //   digitalWrite(LED_BUILTIN_2, HIGH);  // turn the LED on (HIGH is the voltage level)
+  // }
   // Serial.println();
   int calc = calc8421(normalized_values[0], 
                       normalized_values[1],
@@ -287,8 +198,8 @@ void loop() {
                       normalized_values[4],
                       normalized_values[5],
                       normalized_values[6],
-                      normalized_values[7],
-                      sideInterference);
+                      normalized_values[7]
+                      );
   // Serial.print("calc ");
   // Serial.print(calc);
   // Serial.println();
@@ -301,10 +212,17 @@ void loop() {
   error[0] = error[1];
   error[1] = calc;
 
-  float k_p = 0.04; // 0.04
+  float k_p = 0.04; // 0.04  // with 25 speed 0.03
+  if (startTicks < 150){
+    k_p = 0.02;
+  }
   int p = calc * k_p;
 
-  float k_d = 0.3; // 0.3
+  float k_d = 0.25; // 0.3   // with 25 speed 0.2
+  if (startTicks < 150){
+    k_d = 0.1;
+  }
+
   int delta_error = error[1] - error[0];
   int d = delta_error * k_d;
 
@@ -355,11 +273,12 @@ void loop() {
     // digitalWrite(right_dir_pin,LOW);
     // digitalWrite(right_nslp_pin,HIGH);
 
-    analogWrite(left_pwm_pin,25);
-    analogWrite(right_pwm_pin,25);
+    analogWrite(left_pwm_pin,233);  // 120   80 ticks
+    analogWrite(right_pwm_pin,233);
     spinTicks = spinTicks + 1;
-    if (spinTicks > 480){
+    if (spinTicks > 115){
       if (runs == 1){
+        delay(250);  
         spinTicks = 0;
         crossSpeedFlag = 0;
         spinState = 3;
@@ -370,6 +289,9 @@ void loop() {
           runLightShow();
         }
       }else if (runs == 0){
+        analogWrite(left_pwm_pin,0);
+        analogWrite(right_pwm_pin,0);
+        runLightShow();
         spinTicks = 0;
         crossSpeedFlag = 0;
         spinState = 0;
@@ -382,6 +304,7 @@ void loop() {
         digitalWrite(left_nslp_pin,HIGH);
         digitalWrite(right_dir_pin,LOW);
         digitalWrite(right_nslp_pin,HIGH);
+
       }
     }
   } else if (spinState == 0){
@@ -394,15 +317,15 @@ void loop() {
     // digitalWrite(left_nslp_pin,HIGH);
     // digitalWrite(right_dir_pin,LOW);
     // digitalWrite(right_nslp_pin,HIGH);
-    if (startTicks < 100){
-      // analogWrite(left_pwm_pin,20);
-      // analogWrite(right_pwm_pin,20);
-      startTicks = startTicks + 1;
+    if (startTicks < 80){
+      analogWrite(left_pwm_pin,15);
+      analogWrite(right_pwm_pin,15);
+      
     }else{
-      // analogWrite(left_pwm_pin,newSpeedL);
-      // analogWrite(right_pwm_pin,newSpeedR);
+      analogWrite(left_pwm_pin,newSpeedL);
+      analogWrite(right_pwm_pin,newSpeedR);
     }
-
+    startTicks = startTicks + 1;
   }
   // else if (spinState == 3){
   //   analogWrite(left_pwm_pin,0);
@@ -420,31 +343,59 @@ void runLightShow() {
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(LED_BUILTIN_2, HIGH);
   digitalWrite(LED_RF, HIGH);
-  delay(500);  // Wait for 500 milliseconds
+  delay(125);  // Wait for 125 milliseconds
 
   // Turn all LEDs off
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(LED_BUILTIN_2, LOW);
   digitalWrite(LED_RF, LOW);
-  delay(500);  // Wait for 500 milliseconds
+  delay(125);  // Wait for 125 milliseconds
 
   // Turn LEDs on in a different pattern
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(250);  // Wait for 250 milliseconds
+  delay(62);  // Wait for 62 milliseconds
   digitalWrite(LED_BUILTIN_2, HIGH);
-  delay(250);  // Wait for 250 milliseconds
+  delay(62);  // Wait for 62 milliseconds
   digitalWrite(LED_RF, HIGH);
-  delay(250);  // Wait for 250 milliseconds
+  delay(62);  // Wait for 62 milliseconds
 
   // Turn LEDs off in reverse order
   digitalWrite(LED_RF, LOW);
-  delay(250);  // Wait for 250 milliseconds
+  delay(62);  // Wait for 62 milliseconds
   digitalWrite(LED_BUILTIN_2, LOW);
-  delay(250);  // Wait for 250 milliseconds
+  delay(62);  // Wait for 62 milliseconds
   digitalWrite(LED_BUILTIN, LOW);
-  delay(250);  // Wait for 250 milliseconds
+  delay(62);  // Wait for 62 milliseconds
 
   // Add more patterns as needed
+  
+  // Turn all LEDs on
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN_2, HIGH);
+  digitalWrite(LED_RF, HIGH);
+  delay(125);  // Wait for 125 milliseconds
+
+  // Turn all LEDs off
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN_2, LOW);
+  digitalWrite(LED_RF, LOW);
+  delay(125);  // Wait for 125 milliseconds
+
+  // Turn LEDs on in a different pattern
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(62);  // Wait for 62 milliseconds
+  digitalWrite(LED_BUILTIN_2, HIGH);
+  delay(62);  // Wait for 62 milliseconds
+  digitalWrite(LED_RF, HIGH);
+  delay(62);  // Wait for 62 milliseconds
+
+  // Turn LEDs off in reverse order
+  digitalWrite(LED_RF, LOW);
+  delay(62);  // Wait for 62 milliseconds
+  digitalWrite(LED_BUILTIN_2, LOW);
+  delay(62);  // Wait for 62 milliseconds
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(62);  // Wait for 62 milliseconds
 }
 
 // void  ChangeWheelSpeeds(int initialLeftSpd, int finalLeftSpd, int initialRightSpd, int finalRightSpd) {
